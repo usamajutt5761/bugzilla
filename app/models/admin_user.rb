c0a2e4124f67e317495250eaff5dea2e317ca5d8
usamajutt5761 <usamajutt5761@gmail.com>
2022-03-28 12:10:04 +0500

    active admin role column added

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,6 @@
 class AdminUser < ApplicationRecord
+  enum role: [:project_manager, :developer, :qa, :admin]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
