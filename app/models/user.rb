fde79562a845797e0b38b46866430aef60f5164a
usamajutt5761 <usamajutt5761@gmail.com>
2022-03-29 17:39:39 +0500

    user model and edit user added

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+
+  enum role: [:project_manager, :developer, :qa]
+
+  validates :role, presence: :true
+  
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # has_one_attached :avatar 
+  # # do |attachable|
+  # #   attachable.variant :thumb, resize_to_limit: [100, 100]
+  # # end
+  
+  # #  :add_default_avatar  on: %i[create update]
+
+  # def avatar_thumbnail
+  #   if avatar.attached?
+  #       avatar.variant(resize: "100x100!")
+  #   else 
+  #     after_commit "/default_profile.jpg"
+  #   end
+  # end  
+
+
+  private
+
+  # def add_default_avatar
+  #   unlesss avatar.attached?
+  #    avatar.attach(
+  #      io: File.open(
+  #        Rails.root.join('app', 'assets', 'images', 'default_profile.jpg')
+  #       )
+  #       , filename: 'deault_profile.jpg', content_type: 'image/jpg')
+  #   end
+  # end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  resources :users
+  ActiveAdmin.routes(self)
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+
+  devise_scope :user do
+    get '/sign-in' => "devise/sessions#new", :as => :login
+  end
+
+  
+end
